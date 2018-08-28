1b3b72fb5bcb91444702ec9648c542752376f355
Robin Millette <robin@millette.info>
2018-08-28 01:12:18 -0400

    Catch throttling errors.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // npm
 const fetch = require('isomorphic-unfetch')
 const deburr = require('lodash.deburr')
+const pThrottle = require('p-throttle')
 
 class GeocoderXYZ {
   constructor (opts) {
@@ -14,31 +15,39 @@ class GeocoderXYZ {
       this.baseUrl = `${this.baseUrl}/`
     }
     this.token = opts.token ? `&auth=${opts.token}` : ''
+    this.query = pThrottle(this.query.bind(this), 1, 1000)
   }
 
-  async forward (y) {
-    y = deburr(y || '')
+  async query (q) {
     let err
-    const response = await fetch(`${this.baseUrl}${y}?json=1${this.token}`, {
+    const response = await fetch(q, {
       headers: {
+        'user-agent': 'millette/geocode-xyz client 0.0.0',
         accept: 'application/json; charset=utf-8'
       }
     })
 
-    // istanbul ignore if
-    if (response.status !== 200) {
+    // We get a 403 when throttled with a json error response too
+    if (response.status !== 200 && response.status !== 403) {
       err = new Error(response.statusText)
       err.status = response.status
+      err.headers = Array.from(response.headers)
       throw err
     }
+
     const json = await response.json()
     if (json.error) {
-      err = new Error(json.error.description)
+      // When throttled, the error is in message instead of description
+      err = new Error(json.error.description || json.error.message)
       err.code = json.error.code
       err.status = parseInt(json.error.code, 10)
       throw err
     }
-    if (!json.standard || !json.longt || !json.latt) {
+
+    const { url, headers, status, statusText } = response
+    const headersArray = Array.from(headers)
+
+    if (typeof json.latt !== 'string' || typeof json.longt !== 'string') {
       err = new Error('Unexpected response. Verify the baseUrl.')
       err.response = json
       throw err
@@ -46,12 +55,79 @@ class GeocoderXYZ {
 
     json.longt = parseFloat(json.longt)
     json.latt = parseFloat(json.latt)
-    json.standard.confidence = parseFloat(json.standard.confidence)
-    const { url, headers, status, statusText } = response
+    if (json.standard && typeof json.standard.confidence === 'string') {
+      json.standard.confidence = parseFloat(json.standard.confidence)
+    } else if (typeof json.confidence === 'string') {
+      json.confidence = parseFloat(json.confidence)
+    } else {
+      err = new Error('Unexpected response. Verify the baseUrl.')
+      err.response = json
+      throw err
+    }
+
+    if (typeof json.inlongt === 'string') {
+      json.inlongt = parseFloat(json.inlongt)
+    }
+    if (typeof json.inlatt === 'string') {
+      json.inlatt = parseFloat(json.inlatt)
+    }
+
+    return { json, url, headers, headersArray, status, statusText }
+  }
+
+  async backward ({ longt, latt }) {
+    if (typeof longt !== 'number' || typeof latt !== 'number') {
+      throw new Error('Missing `longt` or `latt`. Both are required.')
+    }
+    const {
+      json,
+      url,
+      headers,
+      headersArray,
+      status,
+      statusText
+    } = await this.query(`${this.baseUrl}${latt},${longt}?json=1${this.token}`)
+
+    if (
+      typeof json.inlatt !== 'number' ||
+      typeof json.inlongt !== 'number' ||
+      typeof json.latt !== 'number' ||
+      typeof json.longt !== 'number'
+    ) {
+      const err = new Error('Unexpected response. Verify the baseUrl.')
+      err.response = json
+      throw err
+    }
+
     return {
       url,
       headers,
-      headersArray: Array.from(headers),
+      headersArray,
+      status,
+      statusText,
+      json
+    }
+  }
+
+  async forward (y) {
+    y = deburr(y || '')
+    const {
+      json,
+      url,
+      headers,
+      headersArray,
+      status,
+      statusText
+    } = await this.query(`${this.baseUrl}${y}?json=1${this.token}`)
+    if (!json.standard) {
+      const err = new Error('Unexpected response. Verify the baseUrl.')
+      err.response = json
+      throw err
+    }
+    return {
+      url,
+      headers,
+      headersArray,
       status,
       statusText,
       json
